54b3d7abe2bea2f5d225ae69ae166bf9d24e4046
Ritik <ritiktyagi1.618@gmail.com>
2024-08-03 18:02:39 +0530

    added documentation

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -17,12 +17,33 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView, TokenVerifyView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Little Lemon API",
+        default_version='v1',
+        description="API for Little Lemon Restaurant",
+        terms_of_service="",
+        contact=openapi.Contact(email="contact@littlelemon.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('LittleLemonAPI.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/',include('djoser.urls.authtoken')),
+
+    # Swagger UI
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh' ),
     # path('api/token/blacklist', TokenBlacklistView.as_view(), name='token_blacklist'),
--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('categories/', views.CategoriesView.as_view()),
     path('categories/<int:pk>', views.SingleCategoriesView.as_view()),
